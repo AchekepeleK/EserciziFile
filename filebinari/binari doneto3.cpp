4b23af36d61c8dede470e0943eb8c35c4f08e5c8
Marcello Missiroli <prof.missiroli@gmail.com>
2017-10-19 17:40:18 +0200

    Aggiunti commenti per @doneto3

--- filebinari/binari doneto3.cpp
+++ filebinari/binari doneto3.cpp
@@ -13,9 +13,11 @@ carta c;
 
 void mazzo(FILE *f){
 	strncpy(c.seme,"denari",sizeof(c.seme)-1);
+	//Questa è una cosa orribile.Stampa sizeof(c.seme) e ne riparliamo. 
 	for(int i=1;i<11;i++){c.valore=i;
 		fwrite(&c,sizeof(c),1,f);
 	}
+	//Perchè un ciclo da 1 a 11? 
 	strncpy(c.seme,"spade",sizeof(c.seme)-1);
 	for(int i1=1;i1<11;i1++){c.valore=i1;
 		fwrite(&c,sizeof(c),1,f);
@@ -32,6 +34,11 @@ void mazzo(FILE *f){
 /*
 
       NON HO CAPITO COME FARE
+      
+      Estrai un numero casuale da 0 a 39 (Posiziona prima carta)
+      Estrai un secondo numero casuale da 0 a 39 (Posiziona seconda carta)
+      Scambia il contenuto del file nelle posizione stabilite. 
+      -- Questa non la puoi fare con un unica operazione, ma ALMENO TRE operazioni...Ricordati come si scambiano due dati un un'array e adatta il metodo al file. 
 
 void mischia(FILE *f){
 	for(int i=0;i<40;i++){
@@ -43,8 +50,6 @@ carta ultima(FILE *f){
 	fread(&c,sizeof(carta),1,f);
 	cout<<c.valore<<" "<<c.seme;
 }
-
-
 
 
 
